85a558102403c52c2a61266936a68b159af89fb9
Max Maischein <corion@corion.net>
2020-12-16 08:23:03 +0100

    Add module as description

---
+++ lib/App/videoconf.pm
@@ -0,0 +1,34 @@
+package App::videoconf;
+use 5.012;
+
+our $VERSION = '0.01';
+
+=head1 NAME
+
+App::videoconf - join video conferences from the command line
+
+=head1 PLANNED FEATURES
+
+=over 4
+
+=item *
+
+Launch Jitsi/Zoom/WebEx/GoToMeeting URL and navigate into meeting
+
+=item *
+
+Read CalDAV for next appointment(s) with known/understandable location
+
+=item *
+
+Screen popup one minute before
+
+=item *
+
+Set up camera before launch
+
+=back
+
+=cut
+
+1;
